d54087ad07423a6dec5ca6376fb8a8ab17b04f45
megamoule <31041216+megamoule@users.noreply.github.com>
2017-08-15 13:02:26 -0400

    Update data_movement.js

--- preview/js/data_movement.js
+++ preview/js/data_movement.js
@@ -14,23 +14,32 @@ data_movement = [
         ]
     },
     {
-        title: "Climb",
-        icon: "crags",
-        subtitle: "Cost: 10ft per 5ft",
-        description: "Movement cost: 10ft per 5ft climbed",
+        title: "Jump",
+        icon: "wingfoot",
+        subtitle: "Cost: 5ft per 5ft",
+        description: "Movement cost: 5ft per 5ft jumped",
+        
         reference: "PHB, pg. 182.",
         bullets: [
-            "May involve a Strength (Athletics) check if the climb is difficult"
+            "When you make a standing jump, you can jump only half that distance.",
+            "<b>High Jump</b>",
+            "You leap into the air a number of feet equal to <b>3 + your Strength modifier</b> if you move at least 10 feet on foot immediately before the jump.",
+            "You can extend your arms half your height above yourself during the jump.",
+            "In some circumstances, your DM might allow you to make a Strength (Athletics) check to jump higher than you normally can."
+            "<b>Long Jump</b>",
+            "You cover a number of feet up to your <b>Strength score</b> if you move at least 10 feet on foot immediately before the jump.",
+            "May involve a DC 10 Strength (Athletics) check to clear a low obstacle (no taller than a quarter of the jump's distance). You hit the obstacle on a failed check.",
+            "May involve a DC 10 Dexterity (Acrobatics) check to land on your feet in difficult terrain. You land prone on a failed check."
         ]
     },
     {
-        title: "Swim",
-        icon: "at-sea",
+        title: "Climb/Swim",
+        icon: "crags",
         subtitle: "Cost: 10ft per 5ft",
-        description: "Movement cost: 10ft per 5ft swum",
+        description: "Movement cost: 10ft per 5ft",
         reference: "PHB, pg. 182.",
         bullets: [
-            "May involve a Strength (Athletics) check if the swim is difficult"
+            "May involve a Strength (Athletics) check if the climb/swim is difficult"
         ]
     },
     {
@@ -66,39 +75,14 @@ data_movement = [
         ]
     },
     {
-        title: "High jump",
-        icon: "wingfoot",
-        subtitle: "Cost: 5ft per 5ft",
-        description: "Movement cost: 5ft per 5ft jumped",
-        
-        reference: "PHB, pg. 182.",
+        title: "Grapple move",
+        icon: "grab",
+        subtitle: "Modifier: speed halved",
+        description: "Drag or carry the grappled creature with you",
+        reference: "PHB, pg. 195.",
         bullets: [
-            "You leap into the air a number of feet equal to <b>3 + your Strength modifier</b> if you move at least 10 feet on foot immediately before the jump.",
-            "When you make a standing high jump, you can jump only half that distance.",
-            "You can extend your arms half your height above yourself during the jump.",
-            "In some circumstances, your DM might allow you to make a Strength (Athletics) check to jump higher than you normally can."
-        ]
-    },
-    {
-        title: "Long jump",
-        icon: "wingfoot",
-        subtitle: "Cost: 5ft per 5ft",
-        description: "Movement cost: 5ft per 5ft jumped",
-        reference: "PHB, pg. 182.",
-        bullets: [
-            "You cover a number of feet up to your <b>Strength score</b> if you move at least 10 feet on foot immediately before the jump.",
-            "When you make a standing long jump, you can leap only half that distance",
-            "May involve a DC 10 Strength (Athletics) check to clear a low obstacle (no taller than a quarter of the jump's distance). You hit the obstacle on a failed check.",
-            "May involve a DC 10 Dexterity (Acrobatics) check to land on your feet in difficult terrain. You land prone on a failed check."
-        ]
-    },
-    {
-        title: "Improvise",
-        icon: "juggler",
-        subtitle: "Any stunt not on this list",
-        description: "Perform any movement or stunt you can imagine",
-        bullets: [
-            "When you describe a kind of movement not detailed elsewhere in the rules, the DM tells you whether it is possible and what kind of roll you need to make, if any, to determine success or failure."
+            "If you move while grappling another creature, your speed is halved, unless the creature is two or more sizes smaller than you.",
+            "See the attack action for how to grapple a creature."
         ]
     },
     {
@@ -111,14 +95,12 @@ data_movement = [
         ]
     },
     {
-        title: "Grapple move",
-        icon: "grab",
-        subtitle: "Modifier: speed halved",
-        description: "Drag or carry the grappled creature with you",
-        reference: "PHB, pg. 195.",
+        title: "Improvise",
+        icon: "juggler",
+        subtitle: "Any stunt not on this list",
+        description: "Perform any movement or stunt you can imagine",
         bullets: [
-            "If you move while grappling another creature, your speed is halved, unless the creature is two or more sizes smaller than you.",
-            "See the attack action for how to grapple a creature."
+            "When you describe a kind of movement not detailed elsewhere in the rules, the DM tells you whether it is possible and what kind of roll you need to make, if any, to determine success or failure."
         ]
     }
 ]
